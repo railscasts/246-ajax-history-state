e84cad7de02576dc83af602dd6418cd4798722b8
Giles Bowkett <gilesb@gmail.com>
2011-07-29 15:04:41 -0700

    automatic import from ryanb/railscasts-episodes

---
+++ store/public/javascripts/application.js
@@ -0,0 +1,16 @@
+if (history && history.pushState) {
+  $(function() {
+    $("#products th a, #products .pagination a").live("click", function(e) {
+      $.getScript(this.href);
+      history.pushState(null, document.title, this.href);
+      e.preventDefault();
+    });
+    $("#products_search input").keyup(function() {
+      $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+      history.replaceState(null, document.title, $("#products_search").attr("action") + "?" + $("#products_search").serialize());
+    });
+    $(window).bind("popstate", function() {
+      $.getScript(location.href);
+    });
+  });
+}
